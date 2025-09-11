026acb076bf8eba246897d96199fdb792ef0d2d5
maaaaaaaaad <integral5137@gmail.com>
2025-09-11 16:53:16 +0900

    Add index on `created_at` column in schema creation script

--- db/init/001_schema.sql
+++ db/init/001_schema.sql
@@ -16,5 +16,10 @@ CREATE TABLE IF NOT EXISTS users
     created_at DATETIME
 (
     6
-) NOT NULL
+) NOT NULL,
+    INDEX idx_created_at
+(
+    created_at
+    DESC
+)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE =utf8mb4_0900_ai_ci;
